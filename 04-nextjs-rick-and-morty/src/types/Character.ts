5812a036a85bdd8914d86caef08c7d80b5b7c800
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-04-23 16:58:27 -0600

    feat: types
    there are the response and the character based on the example

---
+++ 04-nextjs-rick-and-morty/src/types/Character.ts
@@ -0,0 +1,23 @@
+export interface Character extends Identifiable {
+    id: number,
+    name: string,
+    status: string,
+    species: string,
+    type: string,
+    gender: string,
+    origin: Place,
+    location: Place,
+    image: string,
+    episode : string[],
+    url: string,
+    created: string
+}
+
+export interface Identifiable {
+    id: number
+}
+
+export interface Place {
+    name: string,
+    url: string
+}
