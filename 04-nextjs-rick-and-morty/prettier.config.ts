bd2dee96c54dcb45e39de4d7e8bc3489f2916a43
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-05-12 16:16:22 -0600

    feat: add Husky

--- 04-nextjs-rick-and-morty/prettier.config.ts
+++ 04-nextjs-rick-and-morty/prettier.config.ts
@@ -4,6 +4,6 @@ const config = {
   semi: false, // decide if you want to add semicolon at the end
   plugins: ['prettier-plugin-tailwindcss'], // help us to change order automatically for Tailwind
   jsxSingleQuote: true, // decide if you want to add single quotes for JSX
-}
+};
 
 export default config
