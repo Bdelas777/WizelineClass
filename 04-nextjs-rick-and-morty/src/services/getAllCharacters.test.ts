d920eeaa5f92acc9c4295fc20e73f57cbe91ed15
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-05-29 11:42:13 -0600

    Fix: problems

--- 04-nextjs-rick-and-morty/src/services/getAllCharacters.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expectTypeOf } from "vitest";
-import { getAllCharacters } from "./getAllCharacters";
-import { waitFor } from "@testing-library/react";
-import { Character } from "@/types/Character";
-
-test("expect getCharacters to return a list of Characters", async () => {
-  const res = await waitFor(() => getAllCharacters());
-  expectTypeOf(res).toMatchTypeOf<Character[]>();
-});
